50ba245958fb557e4dd5c49115ab669f1e04f086
JordyMoos <jordymoos@gmail.com>
2017-05-12 00:12:52 +0200

    Be able to collect the coins

--- Assets/scripts/Coin.cs
+++ Assets/scripts/Coin.cs
@@ -2,15 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Coin : MonoBehaviour {
+public class Coin : MonoBehaviour
+{
+    public static int CoinCount = 0;
 
 	// Use this for initialization
-	void Start () {
-        Debug.Log("Object Created");
+	void Start ()
+    {
+        CoinCount++;
+
+        Debug.Log("Current coint count = " + CoinCount);
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
 		
 	}
+
+    void OnDestroy()
+    {
+        CoinCount--;
+
+        if (CoinCount <= 0)
+        {
+            Debug.Log("We won!");
+        }
+    }
 }
---
+++ Assets/scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public float MaxTime = 60f;
+
+    [SerializeField]
+    private float CountDown = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        CountDown = MaxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDown -= Time.deltaTime;
+        
+        // Restart level if time runs out
+        if (CountDown <= 0)
+        {
+            Debug.Log("Time limit reached");
+
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
